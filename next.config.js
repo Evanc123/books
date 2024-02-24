40a5247b7c4028c9634101f456c6ec1cb2b238d9
Evan Cater <evan.ecater@gmail.com>
2024-02-24 14:19:44 -0500

    feat: no build check

--- next.config.js
+++ next.config.js
@@ -11,6 +11,13 @@ const config = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
 
   /**
